0a5278059b4b6fefe74ddec27711767dd64cbc37
jortegam22 <47358136+jortegam22@users.noreply.github.com>
2020-03-25 20:32:53 +0100

    New main.tf

--- ArquitecturaCompleta/main.tf
+++ ArquitecturaCompleta/main.tf
@@ -5,7 +5,40 @@ resource "azurerm_resource_group" "rg" {
   location = "__rg_location__"
 }
 
-//IoTHub Configuration
+resource "azurerm_storage_account" "sa" {
+  name                     = "__storage_name__"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_eventhub_namespace" "ehns" {
+  name                = "__ns_name__"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "Standard"
+  capacity            = 1
+}
+
+resource "azurerm_eventhub" "eh" {
+  name                = "__eh_name__"
+  namespace_name      = azurerm_eventhub_namespace.ehns.name
+  resource_group_name = azurerm_resource_group.rg.name
+  partition_count     = 2
+  message_retention   = 1
+}
+
+resource "azurerm_eventhub_authorization_rule" "ar" {
+  name                = "__ar_name__"
+  namespace_name      = azurerm_eventhub_namespace.ehns.name
+  eventhub_name       = azurerm_eventhub.eh.name
+  resource_group_name = azurerm_resource_group.rg.name
+
+  listen = false
+  send   = true
+  manage = false
+}
 
 resource "azurerm_iothub" "iothub" {
   name                = "__iothub_name__"
@@ -22,11 +55,6 @@ endpoint {
     type                       = "AzureIotHub.EventHub"
     connection_string          = azurerm_eventhub_authorization_rule.ar.primary_connection_string
     name                       = "__endpoint_name__"
-    /*batch_frequency_in_seconds = 60
-    max_chunk_size_in_bytes    = 10485760
-    container_name             = "__pre_ASA__"
-    encoding                   = "Json"
-    file_name_format           = "{iothub}/{partition}_{YYYY}_{MM}_{DD}_{HH}_{mm}"*/
   }
 
   route {
@@ -36,81 +64,6 @@ endpoint {
     endpoint_names = ["__endpoint_name__"]
     enabled        = true
   }
-}
-
-resource "azurerm_eventhub_authorization_rule" "ar" {
-  name                = "__ar_name__"
-  namespace_name      = azurerm_eventhub_namespace.ehns.name
-  eventhub_name       = azurerm_eventhub.eh.name
-  resource_group_name = azurerm_resource_group.rg.name
-
-  listen = false
-  send   = true
-  manage = false
-}
-
-resource "azurerm_iothub_endpoint_eventhub" "epeh" {
-  resource_group_name = azurerm_resource_group.rg.name
-  iothub_name         = azurerm_iothub.iothub.name
-  name                = "__epeh_name__"
-
-  connection_string = azurerm_eventhub_authorization_rule.ar.primary_connection_string
-}
-
-/*resource "azurerm_iothub_endpoint_storage_container" "epse" {
-  resource_group_name = azurerm_resource_group.rg.name
-  iothub_name         = azurerm_iothub.iothub.name
-  name                = "__epse_name__"
-
-  container_name    = "events"
-  connection_string = azurerm_storage_account.sa.primary_blob_connection_string
-
-  file_name_format           = "{iothub}/{partition}_{YYYY}_{MM}_{DD}_{HH}_{mm}"
-  batch_frequency_in_seconds = 60
-  max_chunk_size_in_bytes    = 10485760
-  encoding                   = "JSON"
-}*/
-
-//Storage Account and Containers Configuration
-
-resource "azurerm_storage_account" "sa" {
-  name                     = "__storage_name__"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-/*resource "azurerm_storage_container" "pre_asa" {
-  name                  = "__pre_ASA__"
-  resource_group_name   = azurerm_resource_group.rg.name
-  storage_account_name  = azurerm_storage_account.sa.name
-  container_access_type = "container"
-}
-
-resource "azurerm_storage_container" "post_asa" {
-  name                  = "__post_ASA__"
-  resource_group_name   = azurerm_resource_group.rg.name
-  storage_account_name  = azurerm_storage_account.sa.name
-  container_access_type = "container"
-}*/
-
-//EventHub Configuration
-
-resource "azurerm_eventhub_namespace" "ehns" {
-  name                = "__ns_name__"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  sku                 = "Standard"
-  capacity            = 1
-}
-
-resource "azurerm_eventhub" "eh" {
-  name                = "__eh_name__"
-  namespace_name      = azurerm_eventhub_namespace.ehns.name
-  resource_group_name = azurerm_resource_group.ehns.name
-  partition_count     = 2
-  message_retention   = 1
 }
 
 //Stream Analytic Configuration
@@ -140,22 +93,6 @@ resource "azurerm_stream_analytics_job" "asa" {
   QUERY
 }
 
-/*resource "azurerm_stream_analytics_stream_input_iothub" "example" {
-  name                         = "__asa_input_name__"
-  stream_analytics_job_name    = azurerm_stream_analytics_job.asa.name
-  resource_group_name          = azurerm_stream_analytics_job.asa.resource_group_name
-  endpoint                     = "messages/events"
-  eventhub_consumer_group_name = "$Default"
-  iothub_namespace             = azurerm_iothub.iothub.name
-  shared_access_policy_key     = azurerm_iothub.iothub.shared_access_policy.0.primary_key
-  shared_access_policy_name    = "iothubowner"
-
-  serialization {
-    type     = "Json"
-    encoding = "UTF8"
-  }
-}*/
-
 resource "azurerm_eventhub_consumer_group" "ehcg" {
   name                = "__ehcg_name__"
   namespace_name      = azurerm_eventhub_namespace.ehns.name
@@ -166,7 +103,7 @@ resource "azurerm_eventhub_consumer_group" "ehcg" {
 resource "azurerm_stream_analytics_stream_input_eventhub" "sainput" {
   name                         = "__asa_input_name__"
   stream_analytics_job_name    = azurerm_stream_analytics_job.asa.name
-  resource_group_name          = azurerm_stream_analytics_job.asa.resource_group_name
+  resource_group_name          = azurerm_resource_group.rg.name
   eventhub_consumer_group_name = azurerm_eventhub_consumer_group.ehcg.name
   eventhub_name                = azurerm_eventhub.eh.name
   servicebus_namespace         = azurerm_eventhub_namespace.ehns.name
