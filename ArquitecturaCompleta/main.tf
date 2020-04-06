1204cddc073b61a3a4b7a8884da2893aa3dc26ef
jortegam22 <47358136+jortegam22@users.noreply.github.com>
2020-04-06 16:44:24 +0200

    Add Resource Group

--- ArquitecturaCompleta/main.tf
+++ ArquitecturaCompleta/main.tf
@@ -5,6 +5,13 @@ provider "azurerm" {
   features {}
 }
 
+//Resource Group Configuration
+
+resource "azurerm_resource_group" "rg" {
+  name     = "__rg_name__"
+  location = "__rg_location__"
+}
+
 //EventHub Configuration
 
 resource "azurerm_eventhub_namespace" "ehns" {
