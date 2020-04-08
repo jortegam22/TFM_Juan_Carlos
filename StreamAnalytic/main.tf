2c5ddf9bc2902502d56bfd0e31901eb1be06b9c4
jortegam22 <47358136+jortegam22@users.noreply.github.com>
2020-04-08 11:38:16 +0200

    Adjust code

--- StreamAnalytic/main.tf
+++ StreamAnalytic/main.tf
@@ -2,8 +2,8 @@
 
 resource "azurerm_stream_analytics_job" "asa" {
   name                                     = "__asa_name__"
-  resource_group_name                      = "__resource_group_name__"
-  location                                 = "__resource_group_location__"
+  resource_group_name                      = "__rg_name__"
+  location                                 = "__rg_location__"
   compatibility_level                      = "1.1"
   data_locale                              = "en-GB"
   events_late_arrival_max_delay_in_seconds = 60
@@ -27,11 +27,11 @@ resource "azurerm_stream_analytics_job" "asa" {
 resource "azurerm_stream_analytics_stream_input_eventhub" "sainput" {
   name                         = "__asa_input_name__"
   stream_analytics_job_name    = azurerm_stream_analytics_job.asa.name
-  resource_group_name          = azurerm_resource_group.rg.name
+  resource_group_name          = "__rg_name__"
   eventhub_consumer_group_name = "__ehcg_name__"
-  eventhub_name                = azurerm_eventhub.eh.name
-  servicebus_namespace         = azurerm_eventhub_namespace.ehns.name
-  shared_access_policy_key     = azurerm_eventhub_namespace.ehns.default_primary_key
+  eventhub_name                = "__eh_name__"
+  servicebus_namespace         = "__ehns_name__"
+  shared_access_policy_key     = "__ehns_access_policy_key__"
   shared_access_policy_name    = "iothubowner"
 
   serialization {
@@ -43,9 +43,9 @@ resource "azurerm_stream_analytics_stream_input_eventhub" "sainput" {
 resource "azurerm_stream_analytics_output_blob" "prodbs" {
   name                      = "__asa_output_name__"
   stream_analytics_job_name = azurerm_stream_analytics_job.asa.name
-  resource_group_name       = azurerm_resource_group.rg.name
-  storage_account_name      = azurerm_storage_account.sa.name
-  storage_account_key       = azurerm_storage_account.sa.primary_access_key
+  resource_group_name       = "__rg_name__"
+  storage_account_name      = "__sa_name__"
+  storage_account_key       = "__sa_key__"
   storage_container_name    = "__post_ASA__"
   path_pattern              = "datos"
   date_format               = "yyyy-MM-dd"
