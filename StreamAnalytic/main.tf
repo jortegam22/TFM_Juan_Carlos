e569f041c1129f6424318cd5e0c834544ea14711
jortegam22 <47358136+jortegam22@users.noreply.github.com>
2020-04-08 12:56:41 +0200

    Change the terraform file

--- StreamAnalytic/main.tf
+++ StreamAnalytic/main.tf
@@ -31,36 +31,37 @@ resource "azurerm_stream_analytics_job" "asa" {
   QUERY
 }
 
-resource "azurerm_stream_analytics_stream_input_eventhub" "sainput" {
-  name                         = "__asa_input_name__"
-  stream_analytics_job_name    = azurerm_stream_analytics_job.asa.name
-  resource_group_name          = "__rg_name__"
-  eventhub_consumer_group_name = "__ehcg_name__"
-  eventhub_name                = "__eh_name__"
-  servicebus_namespace         = "__ehns_name__"
-  shared_access_policy_key     = "__ehns_access_policy_key__"
-  shared_access_policy_name    = "RootManageSharedAccessKey"
-
-  serialization {
-    type     = "Json"
-    encoding = "UTF8"
-  }
-}
-
-resource "azurerm_stream_analytics_output_blob" "prodbs" {
-  name                      = "__asa_output_name__"
-  stream_analytics_job_name = azurerm_stream_analytics_job.asa.name
-  resource_group_name       = "__rg_name__"
-  storage_account_name      = "__sa_name__"
-  storage_account_key       = "__sa_key__"
-  storage_container_name    = "__post_ASA__"
-  path_pattern              = "datos"
-  date_format               = "yyyy-MM-dd"
-  time_format               = "HH"
-
-  serialization {
-    type            = "Json"
-    encoding        = "UTF8"
-    format          = "LineSeparated"
-  }
-}
+#resource "azurerm_stream_analytics_stream_input_eventhub" "sainput" {
+#  name                         = "__asa_input_name__"
+#  stream_analytics_job_name    = azurerm_stream_analytics_job.asa.name
+#  resource_group_name          = "__rg_name__"
+#  eventhub_consumer_group_name = "__ehcg_name__"
+#  eventhub_name                = "__eh_name__"
+#  servicebus_namespace         = "__ehns_name__"
+#  shared_access_policy_key     = "__ehns_access_policy_key__"
+#  shared_access_policy_name    = "RootManageSharedAccessKey"
+#
+#  serialization {
+#    type     = "Json"
+#    encoding = "UTF8"
+#  }
+#}
+#
+#resource "azurerm_stream_analytics_output_blob" "prodbs" {
+#  name                      = "__asa_output_name__"
+#  stream_analytics_job_name = azurerm_stream_analytics_job.asa.name
+#  resource_group_name       = "__rg_name__"
+#  storage_account_name      = "__sa_name__"
+#  storage_account_key       = "__sa_key__"
+#  storage_container_name    = "__post_ASA__"
+#  path_pattern              = "datos"
+#  date_format               = "yyyy-MM-dd"
+#  time_format               = "HH"
+#
+#  serialization {
+#    type            = "Json"
+#    encoding        = "UTF8"
+#    format          = "LineSeparated"
+#  }
+#}
+#
