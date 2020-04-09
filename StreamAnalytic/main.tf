b1a33aac25a6bfa6f7da79ab142c3f961009b921
jortegam22 <47358136+jortegam22@users.noreply.github.com>
2020-04-09 11:08:39 +0200

    Update variables in main.tf

--- StreamAnalytic/main.tf
+++ StreamAnalytic/main.tf
@@ -5,17 +5,11 @@ provider "azurerm" {
   features {}
 }
 
-//Resource Group Information
-
-data "azurerm_resource_group" "rg" {
-  name = var.rg_name
-}
-
 //ASA Configuration
 
 resource "azurerm_stream_analytics_job" "asa" {
   name                                     = var.asa_name
-  resource_group_name                      = azurerm_resource_group.rg.name
+  resource_group_name                      = var.rg_name
   location                                 = var.rg_location
   compatibility_level                      = "1.1"
   data_locale                              = "en-GB"
@@ -38,25 +32,13 @@ resource "azurerm_stream_analytics_job" "asa" {
   QUERY
 }
 
-//EventHubNameSpace Information
-
-data "azurerm_eventhub_namespace" "ehns" {
-  name = var.ehns_name
-}
-
-//EventHub Information
-
-data "azurerm_eventhub" "eh" {
-  name = var.eh_name
-}
-
 resource "azurerm_stream_analytics_stream_input_eventhub" "asainput" {
   name                         = var.asa_input_name
   stream_analytics_job_name    = azurerm_stream_analytics_job.asa.name
-  resource_group_name          = azurerm_resource_group.rg.name
+  resource_group_name          = var.rg_name
   eventhub_consumer_group_name = var.ehcg_name
-  eventhub_name                = azurerm_eventhub.eh.name
-  servicebus_namespace         = azurerm_eventhub_namespace.ehns.name
+  eventhub_name                = var.eh_name
+  servicebus_namespace         = var.ehns_name
   shared_access_policy_key     = var.ehns_access_policy_key
   shared_access_policy_name    = "RootManageSharedAccessKey"
 
@@ -66,25 +48,13 @@ resource "azurerm_stream_analytics_stream_input_eventhub" "asainput" {
   }
 }
 
-//StorageAccount Information
-
-data "azurerm_storage_account" "sa" {
-  name = var.sa_name
-}
-
-//Container Information
-
-data "azurerm_storage_container" "sc" {
-  name = var.post_ASA
-}
-
 resource "azurerm_stream_analytics_output_blob" "asablob" {
   name                      = var.asa_output_name
   stream_analytics_job_name = azurerm_stream_analytics_job.asa.name
-  resource_group_name       = azurerm_resource_group.rg.name
-  storage_account_name      = azurerm_storage_account.sa.name
+  resource_group_name       = var.rg_name
+  storage_account_name      = var.sa_name
   storage_account_key       = var.sa_key
-  storage_container_name    = azurerm_storage_container.name
+  storage_container_name    = var.post_ASA
   path_pattern              = "datos"
   date_format               = "yyyy-MM-dd"
   time_format               = "HH"
